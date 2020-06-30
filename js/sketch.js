963b4ce75b49086f717bd904ff8570a86d64a3a8
audreyannen <60931133+audreyannen@users.noreply.github.com>
2020-06-30 15:05:45 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,13 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
+    gfx = createGraphics(asciiart_width, asciiart_height);
+    gfx.pixelDensity(1);
 }
 
-function draw() { 
+function draw() {
 
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight);
 }
